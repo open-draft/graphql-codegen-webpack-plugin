5930492f3079f3324122238d4b477a5e5919d3f4
Artem Zakharchenko <kettanaito@gmail.com>
2021-05-31 17:27:39 +0200

    Removes logging

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,4 @@
-import * as fs from 'fs'
-import * as path from 'path'
-import { Compilation, Compiler, WebpackPluginInstance } from 'webpack'
+import { Compiler, WebpackPluginInstance } from 'webpack'
 import { generate, loadContext } from '@graphql-codegen/cli'
 
 export interface PluginOptions {
@@ -18,33 +16,10 @@ export class GraphQLCodegenWebpackPlugin implements WebpackPluginInstance {
   public async apply(compiler: Compiler) {
     const codegenContext = await loadContext(this.options.configPath)
 
-    compiler.hooks.beforeCompile.tapPromise(this.name, async (compilation) => {
-      const files = await generate(codegenContext).catch((error) => {
+    compiler.hooks.beforeCompile.tapPromise(this.name, async () => {
+      await generate(codegenContext).catch((error) => {
         console.error('Failed to generate GraphQL types!', error)
       })
-
-      console.log(files)
     })
-  }
-
-  private async addAsset(filePath: string, compilation: Compilation) {
-    const logger = compilation.getLogger(this.name)
-    const absoluteFilePath = path.resolve(
-      compilation.compiler.context,
-      filePath
-    )
-
-    const [fileStats, fileContent] = await Promise.all([
-      fs.promises.stat(filePath),
-      fs.promises.readFile(filePath),
-    ])
-
-    const fileName = path.basename(filePath)
-    // compilation.assets[fileName] = {
-    //   source: () => fileContent,
-    //   size: () => fileStats.size,
-    // }
-
-    return fileName
   }
 }
