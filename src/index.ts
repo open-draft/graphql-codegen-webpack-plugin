5eb180d5f71c3be45ed2730ca4400e42937ea325
Artem Zakharchenko <kettanaito@gmail.com>
2021-05-31 17:37:42 +0200

    Respects webpack watch mode

--- src/index.ts
+++ src/index.ts
@@ -8,18 +8,32 @@ export interface PluginOptions {
 export class GraphQLCodegenWebpackPlugin implements WebpackPluginInstance {
   private name = 'GraphQLCodegenWebpackPlugin'
   private options: PluginOptions
+  private isCodegenEnabled: boolean
 
   constructor(options?: PluginOptions) {
+    this.isCodegenEnabled = false
     this.options = options
   }
 
   public async apply(compiler: Compiler) {
     const codegenContext = await loadContext(this.options.configPath)
+    const codegenConfig = codegenContext.getConfig()
+
+    if (this.isCodegenEnabled) {
+      return
+    }
 
     compiler.hooks.beforeCompile.tapPromise(this.name, async () => {
-      await generate(codegenContext).catch((error) => {
-        console.error('Failed to generate GraphQL types!', error)
+      await generate({
+        ...codegenConfig,
+        watch: compiler.watchMode,
       })
+        .then(() => {
+          this.isCodegenEnabled = true
+        })
+        .catch((error) => {
+          console.error('Failed to generate GraphQL types!', error)
+        })
     })
   }
 }
