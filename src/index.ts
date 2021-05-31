bb8ef666e6bedf9bf66274bda5e9a0a5e3d3b1fc
Artem Zakharchenko <kettanaito@gmail.com>
2021-05-31 17:41:20 +0200

    Runs in silent mode

--- src/index.ts
+++ src/index.ts
@@ -16,6 +16,7 @@ export class GraphQLCodegenWebpackPlugin implements WebpackPluginInstance {
   }
 
   public async apply(compiler: Compiler) {
+    const logger = compiler.getInfrastructureLogger(this.name)
     const codegenContext = await loadContext(this.options.configPath)
     const codegenConfig = codegenContext.getConfig()
 
@@ -26,13 +27,16 @@ export class GraphQLCodegenWebpackPlugin implements WebpackPluginInstance {
     compiler.hooks.beforeCompile.tapPromise(this.name, async () => {
       await generate({
         ...codegenConfig,
+        errorsOnly: true,
         watch: compiler.watchMode,
       })
         .then(() => {
           this.isCodegenEnabled = true
+          logger.info('successfully generated GraphQL types')
         })
         .catch((error) => {
-          console.error('Failed to generate GraphQL types!', error)
+          logger.error('failed to generate GraphQL types')
+          throw error
         })
     })
   }
